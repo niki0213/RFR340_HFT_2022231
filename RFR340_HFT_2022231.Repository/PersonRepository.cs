853d3e9213ccdd60d12e2a11132217b104226049
niki.nagy@stud.uni-obuda.hu <niki.nagy@stud.uni-obuda.hu>
2022-11-25 22:42:15 +0100

    PersonRepository created

---
+++ RFR340_HFT_2022231.Repository/PersonRepository.cs
@@ -0,0 +1,34 @@
+﻿using RFR340_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFR340_HFT_2022231.Repository
+{
+    public class PersonRepository : Repository<Person>, IRepository<Person>
+    {
+        public PersonRepository(LibraryDbContext Library) : base(Library)
+        {
+        }
+
+        public override Person Read(int id)
+        {
+            return Library.Person.FirstOrDefault(t => t.PersonID == id);
+        }
+
+        public override void Update(Person person)
+        {
+            var old = Read(person.PersonID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(person));
+                }
+            }
+            Library.SaveChanges();
+        }
+    }
+}
