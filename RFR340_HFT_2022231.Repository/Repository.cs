512f63d3621680294a6decd9f759c67aed8de48d
niki.nagy@stud.uni-obuda.hu <niki.nagy@stud.uni-obuda.hu>
2022-11-25 22:11:01 +0100

    Reprository class created

---
+++ RFR340_HFT_2022231.Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFR340_HFT_2022231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected LibraryDbContext Library;
+        public Repository(LibraryDbContext Library)
+        {
+            this.Library = Library;
+        }
+        public void Create(T item)
+        {
+            Library.Set<T>().Add(item);
+            Library.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return Library.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            Library.Set<T>().Remove(Read(id));
+            Library.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+    }
+}
