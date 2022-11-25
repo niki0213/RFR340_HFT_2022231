d6966077eeee859263150a29d8e1f80f0207cf71
niki.nagy@stud.uni-obuda.hu <niki.nagy@stud.uni-obuda.hu>
2022-11-25 22:44:13 +0100

    RentRepository created

---
+++ RFR340_HFT_2022231.Repository/RentReporitory.cs
@@ -0,0 +1,34 @@
+﻿using RFR340_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFR340_HFT_2022231.Repository
+{
+    public class RentReporitory : Repository<Rent>, IRepository<Rent>
+    {
+        public RentReporitory(LibraryDbContext Library) : base(Library)
+        {
+        }
+
+        public override Rent Read(int id)
+        {
+            return Library.Rent.FirstOrDefault(t => t.RentID == id);
+        }
+
+        public override void Update(Rent rent)
+        {
+            var old = Read(rent.RentID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(rent));
+                }
+            }
+            Library.SaveChanges();
+        }
+    }
+}
