204fb29df3a81252e5907031e38799e009e3d850
niki.nagy@stud.uni-obuda.hu <niki.nagy@stud.uni-obuda.hu>
2022-11-29 01:10:23 +0100

    Non crud method controller added

---
+++ RFR340_HFT_2022231.Endpoint/Controllers/MethodController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RFR340_HFT_2022231.Logic;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RFR340_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class MethodController : ControllerBase
+    {
+        IBookLogic logic;
+
+        public MethodController(IBookLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<BookLogic.BookReadCount> BookReadCounter()
+        {
+            return this.logic.BookReadCounter();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<BookLogic.BookInfo> HaveRead(int ID)
+        {
+            return this.logic.HaveRead(ID);
+        }
+
+        [HttpGet]
+        public IEnumerable<BookLogic.PublisherInfo> PublishedBooks()
+        {
+            return this.logic.PublishedBooks();
+        }
+
+        [HttpGet]
+        public IEnumerable<BookLogic.NotReturned> DidNotReturned()
+        {
+            return this.logic.DidNotReturned();
+        }
+
+        [ HttpGet("{id}")]
+        public IEnumerable<BookLogic.RentedIt> RentedBy(int bookid)
+        {
+            return this.logic.RentedBy(bookid);
+        }
+    }
+}
